a7bc490d6f701a96bb95635d5d809ba0596bce11
imagine-hussain <hussain.nwz000@gmail.com>
2022-07-30 14:35:03 +1000

    initialise

---
+++ 200.number_of_islands.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        // do dfs and mark neighbour cells as visited if they are islands
+        int island_count = 0;
+        // Get size of grid
+        int m = grid.len();
+    }
+};
