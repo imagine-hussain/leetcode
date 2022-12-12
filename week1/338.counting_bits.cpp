25db1d7ad4ce2d7656284a22fca93423ecf89649
imagine-hussain <hussain.nwz000@gmail.com>
2022-12-13 02:48:09 +1100

    338 - counting bits

---
+++ week1/338.counting_bits.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> vec(n+1);
+
+        for (int i = 1; i <= n; i++)
+            vec[i] = vec[i/2] + i % 2;
+
+        return vec;
+    }
+};
