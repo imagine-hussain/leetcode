6cf2b2c1c853072536100f0f3b730ea79f76eb7c
imagine-hussain <hussain.nwz000@gmail.com>
2022-12-19 13:54:09 +1100

    01 matrix

---
+++ week2/542.01_matrix.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+
+        int rows = mat.size();
+        if (rows == 0) return mat;
+
+        int cols = mat[0].size();
+
+        vector<vector<int>> dp(rows, vector<int>(cols, INT_MAX));
+
+        // From top--left -> bottom right
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    if (i > 0)
+                        dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
+                    if (j > 0)
+                        dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
+                }
+            }
+        }
+
+        // From bottom-right -> top-left
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 0; j--) {
+                if (i < rows - 1)
+                    dp[i][j] = min(dp[i][j], dp[i + 1][j] + 1);
+                if (j < cols - 1)
+                    dp[i][j] = min(dp[i][j], dp[i][j + 1] + 1);
+            }
+        }
+
+        return dp;
+    }
+};
