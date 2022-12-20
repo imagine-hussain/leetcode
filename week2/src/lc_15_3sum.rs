bb5769932bb311a85aeeca44bb64e3bb41834d99
imagine-hussain <hussain.nwz000@gmail.com>
2022-12-20 15:35:32 +1100

    3sum

---
+++ week2/src/lc_15_3sum.rs
@@ -0,0 +1,74 @@
+use crate::Solution;
+
+use std::{cmp, collections::HashSet};
+
+impl Solution {
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut solutions: HashSet<(i32, i32, i32)> = HashSet::new();
+        nums.sort();
+
+        for i in 0..nums.len() - 1 {
+            two_sum(&nums[i..], &mut solutions);
+        }
+
+        solutions
+            .into_iter()
+            .map(|(a, b, c)| vec![a, b, c])
+            .collect()
+    }
+}
+
+pub fn two_sum(nums: &[i32], solutions: &mut HashSet<(i32, i32, i32)>) {
+    if nums.len() < 3 {
+        return;
+    }
+
+    let target = -nums[0];
+    let mut lo = 1;
+    let mut hi = nums.len() - 1;
+
+    while hi > lo {
+        match (nums[hi] + nums[lo]).cmp(&target) {
+            cmp::Ordering::Equal => {
+                checked_solution_insertion(nums[lo], nums[hi], -target, solutions);
+                if nums[lo] == nums[lo + 1] {
+                    lo += 1;
+                } else {
+                    hi -= 1;
+                }
+            }
+            cmp::Ordering::Less => lo += 1,
+            cmp::Ordering::Greater => hi -= 1,
+        };
+    }
+}
+
+fn checked_solution_insertion(a: i32, b: i32, c: i32, solutions: &mut HashSet<(i32, i32, i32)>) {
+    let already_exists = vec![
+        (a, b, c),
+        (a, c, b),
+        (b, a, c),
+        (b, c, a),
+        (c, a, b),
+        (c, b, a),
+    ]
+    .into_iter()
+    .any(|perm| solutions.contains(&perm));
+
+    if !already_exists {
+        solutions.insert((a, b, c));
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let nums = vec![-1, 0, 1, 2, -1, -4];
+        let expected = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+        let actual = Solution::three_sum(nums);
+        assert_eq!(expected, actual);
+    }
+}
--- week2/src/main.rs
+++ week2/src/main.rs
@@ -1,5 +1,8 @@
 
 mod lc_207_course_schedule;
+mod lc_15_3sum;
+
+pub struct Solution {}
 
 fn main() {
     unimplemented!();
